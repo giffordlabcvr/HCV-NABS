95308000107571cd1032b8aa5acea9aac79ba2eb
Josh Singer <josh.singer@glasgow.ac.uk>
2017-12-01 14:40:13 +0000

    only create variation where it doesn't already exist.

--- modules/hcvNabsAnalyseBindingResidues.js
+++ modules/hcvNabsAnalyseBindingResidues.js
@@ -12,11 +12,15 @@ function analyse() {
 				                                "--acRefName", "REF_MASTER_NC_004102", "--featureName", "BR"+br_location_id], 
 				                                {convertTableToObjects:true});
 			});
-			_.each(freqResults, function(freqResult) {
-				glue.inMode("reference/REF_MASTER_NC_004102/feature-location/BR"+br_location_id, function() {
-					glue.command(["create", "variation", "BR_"+freqResult.aminoAcid, "--translationType", "AMINO_ACID",
-					              "Occurance of "+freqResult.aminoAcid+" at binding residue "+br_location_id]);
-					glue.inMode("variation/"+"BR_"+freqResult.aminoAcid, function() {
+			glue.inMode("reference/REF_MASTER_NC_004102/feature-location/BR"+br_location_id, function() {
+				var brVariationNames = tableResultGetColumn(glue.command(["list", "variation", "--whereClause", "name like 'BR%'"], "name"));
+				_.each(freqResults, function(freqResult) {
+					var variationName = "BR_"+freqResult.aminoAcid;
+					if(brVariationNames.indexOf(variationName) < 0) {
+						glue.command(["create", "variation", variationName, "--translationType", "AMINO_ACID",
+						              "Occurance of "+freqResult.aminoAcid+" at binding residue "+br_location_id]);
+					}
+					glue.inMode("variation/"+variationName, function() {
 						glue.command(["set", "field", "hcv_nabs_amino_acid", freqResult.aminoAcid]);
 						glue.command(["create", "pattern-location", freqResult.aminoAcid, "--labeledCodon", br_location_id,br_location_id]);
 						glue.command(["create", "var-almt-note", clade]);
@@ -24,7 +28,7 @@ function analyse() {
 							glue.command(["set", "field", "hcv_nabs_frequency", freqResult.pctMembers]);
 							glue.command(["set", "field", "hcv_nabs_number", freqResult.numMembers]);
 						});
-						
+
 					});
 				});
 			});
