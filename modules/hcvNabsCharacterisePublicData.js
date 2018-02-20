0736a0e1890651397730c423342ed2fc50dba043
Josh Singer <josh.singer@glasgow.ac.uk>
2018-02-20 17:40:47 +0000

    geographical distribution by country rather than region.

--- modules/hcvNabsCharacterisePublicData.js
+++ modules/hcvNabsCharacterisePublicData.js
@@ -1,43 +1,47 @@
 
 
 
-function featureCoverageByRegionAndClade(featureName) {
+function featureCoverageByCountryAndClade(featureName) {
 	var coveragePct = 90;
 
-	var subRegionObjList = glue.tableToObjects(glue.command(["list", "custom-table-row", "who_sub_region", "id", "display_name"]));
+	var countryObjList = glue.tableToObjects(glue.command(["list", "custom-table-row", "who_country", "id", "display_name"]));
+	countryObjList.push({id:"no_country_specified", display_name:"No country specified"});
+	
 	var cladeObjList = [];
+
+	_.each(clades, function(almtName) {
+		var cladeObj = {};
+		glue.logInfo("Clade: "+almtName);
+		glue.inMode("alignment/"+almtName, function() {
+			cladeObj.id = almtName;
+			cladeObj.display_name = glue.command("show property displayName").propertyValueResult.value;
+		});
+		cladeObjList.push(cladeObj);
+	});
+	
 	var whereClause = "sequence.source.name = 'ncbi-curated' and "+
 		"referenceMember = false and "+
 		"fLocNotes.featureLoc.referenceSequence.name = 'REF_MASTER_NC_004102' and "+
 		"fLocNotes.featureLoc.feature.name = '"+featureName+"' and "+
 		"fLocNotes.ref_nt_coverage_pct >= "+coveragePct;
 	
-	_.each(clades, function(almtName) {
-		var cladeObj = {};
-		glue.logInfo("Clade: "+almtName);
-		glue.inMode("alignment/"+almtName, function() {
-			cladeObj["Clade"] = glue.command("show property displayName").propertyValueResult.value;
-		});
-		_.each(subRegionObjList, function(subRegionObj) {
-			glue.logInfo("Sub-region: "+subRegionObj.display_name);
-			glue.inMode("alignment/"+almtName, function() {
+	_.each(countryObjList, function(countryObj) {
+		glue.logInfo("Country: "+countryObj.display_name);
+		_.each(cladeObjList, function(cladeObj) {
+			var cWhereClause;
+			if(countryObj.id == 'no_country_specified') {
+				cWhereClause = whereClause+" and sequence.who_country = null";
+			} else {
+				cWhereClause = whereClause+" and sequence.who_country = '"+countryObj.id+"'";
+			}
+			glue.inMode("alignment/"+cladeObj.id, function() {
 				var result = glue.command(
 						{"count":{"member":{"recursive":"true",
-							"whereClause":
-								whereClause+" and sequence.who_country.who_sub_region = '"+subRegionObj.id+"'"}}});
-				cladeObj[subRegionObj.display_name] = result.countResult.count;
+							"whereClause": cWhereClause}}});
+				countryObj[cladeObj.display_name] = result.countResult.count;
 			});
 		});
-		glue.inMode("alignment/"+almtName, function() {
-			cladeObj["No country specified"] = glue.command(
-					{"count":{"member":{"recursive":"true",
-						"whereClause": whereClause+" and sequence.who_country = null"}}}).countResult.count;
-			cladeObj["Total"] = glue.command(
-					{"count":{"member":{"recursive":"true",
-						"whereClause": whereClause}}}).countResult.count;
-		});
-		cladeObjList.push(cladeObj);
 	});
-	return cladeObjList;
+	return countryObjList;
 }
 
